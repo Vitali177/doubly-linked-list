2ea86bcee148fd739581ce1f54cd97645940e7b9
Vitali <vskrabatun@gmail.com>
2019-09-23 16:11:18 +0300

    feat : add doubly-linked-list task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,141 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
 
-    append(data) {}
+        this.length = 0;
+        this._head = null;
+        this._tail = null;        
+    }
 
-    head() {}
+    append(data) {        
 
-    tail() {}
+        var node = new Node(data);
 
-    at(index) {}
+        if (this.length) {      
 
-    insertAt(index, data) {}
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;    
 
-    isEmpty() {}
+                         
+        } else {
+            this._head = node;
+            this._tail = node;
+        }     
+        this.length++;          
+    }
 
-    clear() {}
+    head() {     
 
-    deleteAt(index) {}
+        return this._head.data;
+    }
 
-    reverse() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    indexOf(data) {}
+    at(index) {
+        var findNode = this._head;
+        for (var i = 0; i < index; i++) {
+            findNode = findNode.next;
+        }
+        return findNode.data;
+    }
+
+    insertAt(index, data) {
+
+        var findNode = this._head;
+
+        for (var i = 0; i < index; i++) {
+            findNode = findNode.next;
+        }
+        var findNodePrev = findNode.prev;
+        var node = new Node(data);
+
+        findNodePrev.next = node;
+        node.prev = findNodePrev;
+
+        node.next = findNode;
+        findNode.prev = node;
+
+    }
+
+    isEmpty() {
+        if (this.length === 0)
+            return true;
+        return false;
+    }
+
+    clear() {
+        
+        this._head.data = null;
+        this._tail.data = null;
+ 
+        this.length = 0;
+       
+    }
+
+    deleteAt(index) {
+       var findNode = this._head;
+
+        for (var i = 0; i < index; i++) {
+            findNode = findNode.next;
+        }
+
+        var findNodePrev = findNode.prev;
+        var findNodeNext = findNode.next;
+
+        findNodePrev.next = findNodeNext;
+        findNodeNext.prev = findNodePrev;
+        
+    }
+
+    reverse() {
+
+        if (this.length > 1) {
+
+            var buff;
+            
+            this._head = this._tail;
+            var nodeFromTail = this._head;
+
+            
+
+            for (var i = 0; i < this.length - 1; i++) {
+
+                buff = nodeFromTail.prev;
+                nodeFromTail.prev = nodeFromTail.next;
+                nodeFromTail.next = buff;
+
+                nodeFromTail = nodeFromTail.next;
+
+            }        
+            buff = nodeFromTail.prev;
+            nodeFromTail.prev = nodeFromTail.next;
+            nodeFromTail.next = buff;
+            
+            this._tail = nodeFromTail;
+
+        }
+        return this;
+        
+    }
+
+    indexOf(data) {
+        var index = 0;
+        var findNode = this._head;
+        while (findNode !== null) {
+
+            if (findNode.data === data)
+                return index;
+
+            index++;
+            findNode = findNode.next;
+        }
+
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
